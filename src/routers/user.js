6f408e309ea72004c61053a4e8c0933e7ea018ca
NathCodeArt <ttrippy.dessin@gmail.com>
2024-04-30 00:27:17 -0500

    arreglo get/id = ok

--- src/routers/user.js
+++ src/routers/user.js
@@ -16,19 +16,17 @@ router.get("/users", async (req, res) => {
 
 // Ruta para obtener un usuario por su ID
 router.get("/users/:id", async (req, res) => {
-  try {
-    const userId = parseInt(req.params.id);
-    const user = await modelUser.findOne({ id: userId });
+  const id = req.params.id;
 
-    if (!user) {
-      return res.status(404).send("Usuario no encontrado");
-    }
-
-    res.status(200).json(user);
-  } catch (error) {
-    console.error("Error al obtener el usuario:", error.message);
-    res.status(500).send("Error al obtener el usuario");
-  }
+  modelUser.findById(id)
+    .then(user => {
+      if (!user)
+        res.status(404).send({ message: "Usuario no encontrado con el ID " + id });
+      else res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).send({ message: "Error al recuperar el usuario con el ID " + id });
+    });
 });
 
 // Ruta para eliminar un usuario por su ID
