23d83ed23d0cab08649c67fe04a7b7112c58e08e
Cecilia Ayala <chechucaro11@gmail.com>
2024-05-10 15:38:28 -0300

    modificacion pantalla, perfil comercial

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ generateBtn.addEventListener('click', () => {
   window.location.href = 'elevator-pich-generado.html';
 });
 
+document.getElementById('elevator-formulario').addEventListener('submit', function(event) {
+  var historia = document.getElementById('historia').value;
+  var descripcion = document.getElementById('descripcion').value;
+
+  if (historia === '' || descripcion === '') {
+      alert('Por favor completa todos los campos obligatorios');
+      event.preventDefault(); // Evita que se envíe el formulario si no están completos los campos
+  }
+});
 function generatePitch() {
   // Lógica para generar el Elevator Pitch con los datos del formulario
 }
